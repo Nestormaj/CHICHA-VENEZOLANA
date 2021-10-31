468f314615c125163f778c1343828827617e600d
Nestormaj <89750713+Nestormaj@users.noreply.github.com>
2021-10-31 16:52:44 -0300

    Add files via upload

---
+++ js/animaciones.js
@@ -0,0 +1,15 @@
+function cambio() {
+
+    document.body.style.backgroundColor = "black";
+    document.getElementById("header").style.backgroundColor = "black";
+    document.getElementById("header").style.color = "white";
+    document.getElementByClass("vinculo").style.backgroundColor = "black";
+    document.getElementByClassName("padre").style.backgroundColor = "black";
+    document.getElementById("section").style.backgroundColor = "black";
+    document.getElementById("footer").style.backgroundColor = "black";
+    
+}
+
+function derecho(){
+    alert("NAME: Nestor Martinez. EMAIL: nestorjose0503@gmail.com");
+}
